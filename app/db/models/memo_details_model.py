e9772361829f8e31a5864281b44d2400ca1640ce
Harish Singla <harishsingla89@gmail.com>
2024-11-04 06:48:53 +0100

    Extend MemoDetails Model

--- app/db/models/memo_details_model.py
+++ app/db/models/memo_details_model.py
@@ -9,6 +9,8 @@ class MemoDetails(Base):
 
     memo_id = Column(Integer, primary_key=True, index=True)
     customer_name = Column(String(100), nullable=False)
+    address = Column(String(200), nullable=False)
+    contact = Column(String(50), nullable=False)
     invoice_date = Column(Date, nullable=False)
     bill_amount = Column(Float, nullable=False)
 
--- app/schemas/memo_details_schema.py
+++ app/schemas/memo_details_schema.py
@@ -8,6 +8,8 @@ from app.schemas.memo_item_schema import MemoItemCreate
 
 class MemoDetailsCreate(BaseModel):
     customerName: str
+    address: str
+    contact: str
     invoiceDate: date
     memoItems: List[MemoItemCreate]
     billAmount: float
