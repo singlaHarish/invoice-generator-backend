1a22c36913c4b7e85bbb93a5d424f772f78a040d
Harish Singla <harishsingla89@gmail.com>
2025-01-19 17:22:24 +0100

    CORS origins

--- app/db/base.py
+++ app/db/base.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+mysqlconnector://root:abcde12345@192.168.0.176:3306/invoicegen"
+#DATABASE_URL = "mysql+mysqlconnector://root:abcde12345@localhost:3306/invoicegen"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
